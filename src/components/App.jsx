73596514bfcebd522d560bc234c667f0926cf952
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2023-09-30 00:00:24 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,87 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+//model.id = nanoid();=> "V1StGXR8_Z5jdHi6B-myT"
+
+import { Component } from 'react';
+import Section from './section/Section';
+import AddContact from './add/AddContact';
+import Contacts from './contacts/Contacts';
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: '',
+  };
+  handleNameChange = e => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+  handleNumberChange = e => {
+    const formattedNumber = this.formatPhoneNumber(e.target.value);
+    this.setState({ number: formattedNumber });
+  };
+  formatPhoneNumber = input => {
+    const value = input.replace(/\D/g, '');
+    const formattedValue = value
+      .slice(0, 9)
+      .replace(/(\d{3})(\d{2})(\d{2})/, '$1-$2-$3');
+
+    return formattedValue;
+  };
+  handleAddContact = () => {
+    const { name, number } = this.state;
+    const letters = /^[A-Za-z]+$/;
+
+    if (letters.test(name) && name.trim() !== '' && number.trim() !== '') {
+      const newContact = {
+        id: nanoid(),
+        name: name.trim(),
+        number: number.trim(),
+      };
+
+      this.setState(prevState => ({
+        name: '',
+        number: '', // Clear the number input
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
+  };
+
+  render() {
+    const { name, contacts, filter, number } = this.state;
+    return (
+      <div>
+        <Section tittle="Name">
+          <input
+            type="text"
+            name="name"
+            required
+            value={name}
+            onChange={this.handleNameChange}
+            maxLength={20}
+          />
+          <input
+            type="tel"
+            name="number"
+            required
+            value={number}
+            onChange={this.handleNumberChange}
+          />
+        </Section>
+
+        <AddContact onAddContact={this.handleAddContact} />
+        <Section tittle="Contacts">
+          <Contacts
+            name={name}
+            contacts={contacts}
+            number={number}
+            id={nanoid()}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/add/AddContact.jsx
@@ -0,0 +1,9 @@
+const AddContact = ({ onAddContact }) => {
+  return (
+    <button type="button" onClick={onAddContact}>
+      Add contact
+    </button>
+  );
+};
+
+export default AddContact;
